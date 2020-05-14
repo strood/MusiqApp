b6b49d23f72f0187bbadfa92d58874e4875bed75
strood <stroodz19@gmail.com>
2020-05-13 18:01:45 -0600

    Altered test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:activation_token) }
-    it { should validate_length_of(:password) }
+    it { should validate_length_of(:password).is_at_least(6) }
   end
 
   describe 'associations' do
